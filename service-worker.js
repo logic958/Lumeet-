832cf9317871e7f25e51eda106f19e863013d118
logic958 <tinocrain@gmail.com>
2026-04-16 12:59:48 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "lumeet-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
